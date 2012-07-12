29fa434d83a75b69da0a0921b0a104b397dc7826
saschalion <saschalion@gmail.com>
2012-07-12 12:38:51 +0700

    Yandex map has been changed

--- assets/js/yandex-map.js
+++ assets/js/yandex-map.js
@@ -1,19 +1,20 @@
-function fid_133999665283018323121(ymaps) {
-    var map = new ymaps.Map("ymaps-map-id_133999665283018323121", {
-        center: [73.39137049999992, 54.97788376683191],
+function fid_134207103620382262664(ymaps) {
+    var map = new ymaps.Map("ymaps-map-id_134207103620382262664", {
+        center: [73.39157700000001, 54.97759290180148],
         zoom: 16,
         type: "yandex#map"
     });
-
     map.behaviors.enable('scrollZoom');
     map.controls
         .add("zoomControl")
         .add("mapTools")
         .add(new ymaps.control.TypeSelector(["yandex#map", "yandex#satellite", "yandex#hybrid", "yandex#publicMap"]));
     map.geoObjects
-        .add(new ymaps.Placemark([73.391371, 54.977279], {
-        balloonContent: "7bits"
-    }, {
-        preset: "twirl#lightblueDotIcon"
-    }));
+        .add(new ymaps.Placemark([73.391577, 54.976803], {
+                balloonContent: "7bits"
+            },
+            {
+                preset: "twirl#lightblueDotIcon"
+            }
+        ));
 };
